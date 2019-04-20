9687b6bb92309ca5502edf8f79c5cf15df68fd5a
CKMoJKE <rogivnazariy@gmail.com>
2019-04-20 22:02:51 +0300

    Create checkAccessibility.js
    add check accessibility async function

---
+++ express-boilerplate/checkAccessibility.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const axeCore = require('axe-core');
+
+module.exports = async function checkAccessibility(url) {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  let axeResult;
+  try {
+    await page.goto(url, { waitUntil: 'load'});
+    const handle = await page.evaluateHandle(`
+			// Inject axe source code
+			${axeCore.source}
+			// Run axe
+			axe.run()
+		`);
+
+    // Get the results from `axe.run()`.
+    axeResult = await handle.jsonValue();
+    // Destroy the handle & return axe results.
+    await handle.dispose();
+    await browser.close();
+  } catch (e) {
+    if (browser) {
+      await browser.close();
+    }
+    throw e;
+  }
+  return axeResult;
+};
